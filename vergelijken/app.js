c48ee65cb7618070361a26098a9fdb068e10df0d
TomRoskam <35801579+TomRoskam@users.noreply.github.com>
2018-09-21 11:13:09 +0200

    vergelijken

---
+++ vergelijken/app.js
@@ -0,0 +1,32 @@
+let integer5 = 5; //Dit is gelijk
+let double = 5.0;
+
+if ( integer5 == double) {
+    console.log("Dit is gelijk");
+}
+
+else {
+    console.log("Dit is niet gelijk");
+}
+
+let integer10 = 10; //Dit is gelijk
+let string10 = "10";
+
+if ( integer10 == string10) {
+    console.log("Dit is gelijk");
+}
+
+else {
+    console.log("Dit is niet gelijk");
+}
+
+let integer20 = 20; //Dit is niet gelijk
+let stringtekst = "Hoe werkt dit?";
+
+if ( integer20 == stringtekst) {
+    console.log("Dit is gelijk");
+}
+
+else {
+    console.log("Dit is niet gelijk");
+}
