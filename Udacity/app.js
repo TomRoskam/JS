64800f5fce67fde22422c58336b4606bfdf97315
TomRoskam <tomroskam38@gmail.com>
2018-10-19 09:33:40 +0200

    udacity

---
+++ Udacity/app.js
@@ -0,0 +1,21 @@
+var education = 'no high school diploma';
+
+// set the value of this based on a person's education
+var salary = "234234/year";
+
+// your code goes here
+if (education == 'no high school diploma') {
+    console.log(`In 2015, a person with ${education} earned a average of ${salary}`);
+} else if (education == 'a high school diploma') {
+    console.log(`In 2015 a person with ${education} earned a average of ${salary}`);
+} else if (education == 'an Associates degree') {
+    console.log(`In 2015 a person with ${education} earned a average of ${salary}`);
+} else if (education == 'a Bachelors degree') {
+    console.log(`In 2015 a person with ${education} earned a average of ${salary}`);
+} else if (education == 'a Masters degree') {
+    console.log(`In 2015 a person with ${education} earned a average of ${salary}`);
+} else if (education == 'a Professional degree') {
+    console.log(`In 2015 a person with ${education} earned a average of ${salary}`);
+} else {
+    console.log(`In 2015 a person with ${education} earned a average of ${salary}`);
+}
